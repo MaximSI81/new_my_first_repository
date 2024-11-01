d89b04c0e234bf37f7007e49f86a0d9c6f07ca65
Maxim Safonov <safonovmio@gmail.com>
2024-11-01 23:47:07 +0300

    new_lesson_json1

--- lesson_json.py
+++ lesson_json.py
@@ -7,7 +7,6 @@ data_price = []
 data_descr = []
 data = []
 for page in range(1, 5):
-
     url = f'https://parsinger.ru/html/index4_page_{page}.html'
     session = requests.Session()
     response = session.get(url=url)
@@ -19,19 +18,20 @@ for page in range(1, 5):
     data_name += [n.text.strip() for n in name]
     price = soup.find_all('p', class_='price')
     data_price += [p.text for p in price]
-    description = soup.find_all('div', class_='description')
-    for j in description:
-        data_descr += [i.text.split(':')[1].strip() for i in j.find_all('li')]
+    data_descr += [list(map(lambda x: x.text.split(': ')[1].strip(), i.find_all('li'))) for i in
+                   soup.find_all('div', class_='description')]
 
-for n, p, index in zip(data_name, data_price, range(0, 128, 4)):
+
+for n, d, p in zip(data_name, data_descr, data_price):
     data.append({
         "Наименование": n,
-        "Бренд": data_descr[index:index + 4][0],
-        "Форм-фактор": data_descr[index:index + 4][1],
-        "Ёмкость": data_descr[index:index + 4][2],
-        "Объем буферной памяти": data_descr[index:index + 4][3],
+        "Бренд": d[0],
+        "Форм-фактор": d[1],
+        "Ёмкость": d[2],
+        "Объем буферной памяти": d[3],
         "Цена": p
     })
 
 with open('res.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
+
